6ff512c8dd88ad06edd0b0319f85b328d9d9ca2d
Dan Erdelt <erd@conceptual.ca>
2022-05-02 16:18:44 -0400

    install scripts

--- install/install.cmd
+++ install/install.cmd
@@ -26,5 +26,8 @@ winget list Mozilla.Firefox || winget install Mozilla.Firefox --accept-package-a
 winget list Mozilla.Firefox.DeveloperEdition && winget upgrade Mozilla.Firefox.DeveloperEdition --accept-package-agreements --accept-source-agreements
 winget list Mozilla.Firefox.DeveloperEdition || winget install Mozilla.Firefox.DeveloperEdition --accept-package-agreements --accept-source-agreements
 
+wsl curl -s --ssl https://raw.githubusercontent.com/cpbuildtools/devcontainer-ngdotnet/release/latest/install/install_wsl.sh -o install.sh
+wsl chmod +x install.sh
+wsl ./install.sh
 
-wsl https://raw.githubusercontent.com/cpbuildtools/devcontainer-ngdotnet/release/latest/install/install_wsl.sh
+wsl --cd ~ code ./development
--- install/install_wsl.sh
+++ install/install_wsl.sh
@@ -1,10 +1,99 @@
 #!/bin/bash
 
+curr_name=$(git config --global user.name)
+
+if [ -z "${curr_name}" ] 
+then
+    echo "Enter your Name: "
+    read name
+else 
+    echo "Enter your Name [${curr_name}]: "
+    read name
+    if [ -z "${name}" ] 
+    then
+        name=${curr_name}
+    fi
+fi
+
+if [ -z "${name}" ] 
+then
+    echo "Error"
+    exit 1
+fi
 
 
+curr_email=$(git config --global user.email)
 
-echo "Enter Your Name: "
-read name
+if [ -z "${curr_name}" ] 
+then
+    echo "Enter your Email: "
+    read email
+else 
+    echo "Enter your Email [${curr_email}]: "
+    read email
+    if [ -z "${email}" ] 
+    then
+        email=${curr_email}
+    fi
+fi
 
-echo "Name ${name}"
+if [ -z "${email}" ] 
+then
+    echo "Error"
+    exit 1
+fi
 
+curr_username=${GITHUB_USER}
+
+if [ -z "${curr_username}" ] 
+then
+    echo "Enter your Github Username: "
+    read username
+else 
+    echo "Enter your Github Username [${curr_username}]: "
+    read username
+    if [ -z "${username}" ] 
+    then
+        username=${curr_username}
+    fi
+fi
+
+if [ -z "${username}" ] 
+then
+    echo "Error"
+    exit 1
+fi
+
+curr_token=${GITHUB_TOKEN}
+
+if [ -z "${curr_token}" ] 
+then
+    echo "Enter your Github Persional Access Token: "
+    read token
+else 
+    echo "Enter your Github Persional Access Token [${curr_token}]: "
+    read token
+    if [ -z "${token}" ] 
+    then
+        token=${curr_token}
+    fi
+fi
+
+if [ -z "${token}" ] 
+then
+    echo "Error"
+    exit 1
+fi
+
+
+git config --global --replace-all user.name "${name}"
+git config --global --replace-all user.email "${email}"
+
+setx.exe GITHUB_USER ${username}
+setx.exe GITHUB_TOKEN ${token}
+setx.exe WSLENV GITHUB_USER:GITHUB_TOKEN
+
+GITHUB_USER=${username}
+GITHUB_TOKEN=${token}
+
+mkdir -p ~/development
