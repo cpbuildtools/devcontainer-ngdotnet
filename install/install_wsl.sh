ea1a897b1cee51f01686cec725a160555402d531
Dan Erdelt <erd@conceptual.ca>
2022-05-03 21:15:32 -0400

    ...

--- install/install_wsl.sh
+++ install/install_wsl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-curl https://raw.githubusercontent.com/creationix/nvm/release/latest/install.sh | bash
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
