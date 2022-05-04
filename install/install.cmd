8e1bfb6f8e0bd761e7949075d01c69192ddc2a81
Dan Erdelt <erd@conceptual.ca>
2022-05-04 14:22:21 -0400

    ...

--- install/install.cmd
+++ install/install.cmd
@@ -5,9 +5,13 @@ wsl --set-default-version 2
 
 winget list Canonical.Ubuntu.2004 || winget install Canonical.Ubuntu.2004 --accept-package-agreements --accept-source-agreements
 
+@echo off
 for /f "delims=" %%a in ('dir /b %userprofile%\AppData\Local\Packages\CanonicalGroupLimited.Ubuntu20.04onWindows*') do set "ubuntuFile=%%a"
-start /WAIT %userprofile%\AppData\Local\Microsoft\WindowsApps\%ubuntuFile%\ubuntu2004.exe > nul
-
+start %userprofile%\AppData\Local\Microsoft\WindowsApps\%ubuntuFile%\ubuntu2004.exe
+echo "Follow the instruction in the ubunto window and then press any key"
+pause
+wsl --shutdown
+@echo on
 
 rem make sure to select ububto as the default wsl
 
