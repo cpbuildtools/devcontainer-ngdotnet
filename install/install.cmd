1421b9e63bf39608bce4289a2e46d929889bf233
Dan Erdelt <erd@conceptual.ca>
2022-05-04 14:07:42 -0400

    run ubuntu

--- install/install.cmd
+++ install/install.cmd
@@ -5,6 +5,10 @@ wsl --set-default-version 2
 
 winget list Canonical.Ubuntu.2004 || winget install Canonical.Ubuntu.2004 --accept-package-agreements --accept-source-agreements
 
+for /f "delims=" %a in ('dir /b %userprofile%\AppData\Local\Packages\CanonicalGroupLimited.Ubuntu20.04onWindows*') do set "ubuntuFile=%a"
+start /WAIT %userprofile%\AppData\Local\Microsoft\WindowsApps\%ubuntuFile%\ubuntu2004.exe
+
+
 rem make sure to select ububto as the default wsl
 
 wsl --cd ~ curl --ssl https://raw.githubusercontent.com/cpbuildtools/devcontainer-ngdotnet/release/latest/install/install_wsl.sh -o install.sh
