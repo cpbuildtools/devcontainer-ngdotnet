c465e2694f59439f23385ab6abfb6fdd0a5dda0d
Dan Erdelt <erd@conceptual.ca>
2022-05-03 22:08:33 -0400

    ...

--- install/initialize_wsl.sh
+++ install/initialize_wsl.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install 16
+
+nvm use 16
+
+npm i -g pnpm
+
 export PNPM_HOME="$HOME/.local/share/pnpm"
 export PATH="$PNPM_HOME:$PATH"
 
