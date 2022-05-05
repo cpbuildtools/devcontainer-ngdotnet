ff68313fdbeed013b76826a5b9a132be2faeb096
Dan Erdelt <erd@conceptual.ca>
2022-05-05 13:47:48 -0400

    ...

--- install/installer/util/cmd.ts
+++ install/installer/util/cmd.ts
@@ -3,34 +3,44 @@ import { spawn } from 'child_process';
 
 export function exec(cmd: string): Promise<number> {
     return new Promise((res, rej) => {
-        const child = spawn(cmd, { shell: true, stdio: 'inherit' });
-        child.on('exit', (code) => {
-            res(code ?? 0);
-        });
+        try {
+
+            const child = spawn(cmd, { shell: true, stdio: 'inherit' });
+            child.on('exit', (code) => {
+                res(code ?? 0);
+            });
+        }
+        catch (e) {
+            rej(e);
+        }
     });
 }
 
 
 export function run(cmd: string): Promise<string> {
     return new Promise((res, rej) => {
-        let data:string[] = [];
+        try {
+            let data: string[] = [];
 
-        const child = spawn(cmd, { shell: true, stdio: 'pipe' });
-        child.on('exit', (code) => {
-            let d = data.join('');
-            if(code === 0){
-                res(d);
-            }
-            else {
-                rej({
-                    code, 
-                    data: d
-                });
-            }
-        });
+            const child = spawn(cmd, { shell: true, stdio: 'pipe' });
+            child.on('exit', (code) => {
+                let d = data.join('');
+                if (code === 0) {
+                    res(d);
+                }
+                else {
+                    rej({
+                        code,
+                        data: d
+                    });
+                }
+            });
 
-        child.stdout?.on('data', (d)=>{
-            data.push(d.toString('utf-8'));
-        })
+            child.stdout?.on('data', (d) => {
+                data.push(d.toString('utf-8'));
+            })
+        } catch (e) {
+            rej(e);
+        }
     });
 }
