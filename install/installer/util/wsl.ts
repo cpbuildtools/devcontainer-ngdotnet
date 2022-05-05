c0e8eeb65dc6b311a23159e4b2bd73abe7c3a58c
Dan Erdelt <erd@conceptual.ca>
2022-05-05 14:07:05 -0400

    ...

--- install/installer/util/wsl.ts
+++ install/installer/util/wsl.ts
@@ -3,7 +3,9 @@ import { run } from './cmd.js';
 
 export async function translateWindowsPath(path: string): Promise<string> {
     console.log('translateWindowsPath:', path);
-    return await run(`wslpath -a -u "${path}"`);
+    const result = await run(`wslpath -a -u "${path}"`);
+    console.log('transladPath:', result);
+    return result;
 }
 export async function translateWslPath(path: string): Promise<string> {
     return await run(`wslpath -a -w "${path}"`);
