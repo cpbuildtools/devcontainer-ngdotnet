3ddf3ecc2b54cf712acb9165ef3919d082b5919b
Dan Erdelt <erd@conceptual.ca>
2022-05-05 16:03:14 -0400

    ...

--- install/installer/util/docker.ts
+++ install/installer/util/docker.ts
@@ -1,5 +1,5 @@
 import chalk from 'chalk';
-import { exec } from './cmd.js';
+import { exec, run } from './cmd.js';
 import { translateWindowsPath } from './wsl.js';
 
 export async function dockerLogin(url: string, user: string, token: string) {
@@ -9,15 +9,23 @@ export async function dockerLogin(url: string, user: string, token: string) {
 }
 
 export async function getDockerDesktopPath(){
-    
     const path = await translateWindowsPath('C:\\Program Files\\Docker\\Docker');
-   
     return path;
 }
 export async function startDockerDesktop() {
     try{
         const cmd = `"${await getDockerDesktopPath()}/Docker Desktop.exe" &`;
         console.log('startDockerDesktop:', cmd);
+        let started = false;
+        while(!started){
+            try{
+                const r = await run('docker --version');
+                console.log('ver:', r);
+                started = true;
+            }catch(e){
+                console.log(e)
+            }
+        }
         await exec(cmd);
     }catch(e){
         console.error(e);
