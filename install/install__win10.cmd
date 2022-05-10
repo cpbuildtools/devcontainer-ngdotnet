0e4647f72de8414968a02307532e6c5722fa62e6
Dan Erdelt <erd@conceptual.ca>
2022-05-09 21:04:44 -0400

    win 10 installer

--- install/install__win10.cmd
+++ install/install__win10.cmd
@@ -1,3 +1,34 @@
-@echo off
-curl --ssl https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -o uwsl.msi && uwsl.msi
-wsl --install
+@echo off 
+wsl -l -q 2> nul || goto :enableWSL
+
+echo Downloading wsl update...
+curl --ssl https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi > uwsl.msi && uwsl.msi
+wsl --update 2> nul
+
+wsl --shutdown
+wsl --set-default-version 2
+
+curl --ssl https://raw.githubusercontent.com/cpbuildtools/devcontainer-ngdotnet/release/latest/install/install_wsl.cmd > install_wsl.cmd && install_wsl.cmd
+
+goto end
+
+:enableWSL
+
+echo Enabling Windows Subsystem Linux (this could take a while)...
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
+echo Enabling Virtual Machine Platform (this could also take a while)...
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+echo :
+echo :
+echo :
+echo Enabled Windows Subsystem for Linux. Please restart your computer and run this script again.
+echo :
+echo :
+echo :
+pause
+
+goto end
+
+:end
--- install/install_win11.cmd
+++ install/install_win11.cmd
@@ -1,33 +1,9 @@
 @echo off
+
 wsl --install
 wsl --update
 wsl --shutdown
 wsl --set-default-version 2
 
-winget list Canonical.Ubuntu.2004 || winget install Canonical.Ubuntu.2004 --accept-package-agreements --accept-source-agreements
-
-rem Get the file name of the ubuntu executable
-for /f "delims=" %%a in ('dir /b %userprofile%\AppData\Local\Packages\CanonicalGroupLimited.Ubuntu20.04onWindows*') do set "ubuntuFile=%%a"
-start %userprofile%\AppData\Local\Microsoft\WindowsApps\%ubuntuFile%\ubuntu2004.exe
-echo
-echo
-echo Complete the ubuntu setup in the ubuntu terminal. Once ubuntu is set up return to this window.
-echo
-echo
-pause
-
-wsl -t Ubuntu-20.04
-
-wsl -d Ubuntu-20.04 --cd ~ curl --ssl https://raw.githubusercontent.com/cpbuildtools/devcontainer-ngdotnet/release/latest/install/install_wsl.sh -o install.sh
-wsl -d Ubuntu-20.04 --cd ~ chmod +x install.sh
-wsl -d Ubuntu-20.04 --cd ~ ./install.sh --appdata="%appdata%"
-
-wsl -t Ubuntu-20.04
-
-wsl -d Ubuntu-20.04 --cd ~ curl --ssl https://raw.githubusercontent.com/cpbuildtools/devcontainer-ngdotnet/release/latest/install/initialize_wsl.sh -o initialize.sh
-wsl -d Ubuntu-20.04 --cd ~ chmod +x initialize.sh
-wsl -d Ubuntu-20.04 --cd ~ ./initialize.sh --appdata="%appdata%" 
-
-wsl -t Ubuntu-20.04
-
-@echo on
+ curl --ssl https://raw.githubusercontent.com/cpbuildtools/devcontainer-ngdotnet/release/latest/install/install_wsl.cmd > install_wsl.cmd && install_wsl.cmd
+ 
